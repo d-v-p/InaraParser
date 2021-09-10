3c1590e23da7428010c4ed98e49db546ec97c066
dvp <dvp@spb-team.com>
2021-09-10 12:39:25 +0300

    Refactoring

--- inaraparser.go
+++
@@ -1,9 +0,0 @@
-package inaraParser
-
-import (
-	"github.com/d-v-p/InaraParser/app/commodity"
-)
-
-func FindCommodityBestPrice(commodityName string, refSystemName string, maxDistance int, landingPad string, itemsQuantity int) commodity.CommodityData {
-	return commodity.GetBestCommodityPrice(commodityName, refSystemName, maxDistance, landingPad, itemsQuantity)
-}
